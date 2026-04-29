50cc317a477747c18c00fd66db33689d2a30800f
Emii-lia <fiaromiangaly@gmail.com>
2026-04-29 10:44:12 +0300

    refactor(i18n): optimizing i18n config declaration

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -7,29 +7,32 @@ use i18nrs::yew::{I18nProvider, I18nProviderConfig};
 pub(crate) use routes::*;
 pub(crate) use page::*;
 
-use yew::{component, html, Html};
+use yew::{component, html, use_memo, Html};
 use yew_router::prelude::{BrowserRouter, Switch};
 
 #[component(App)]
 pub fn app() -> Html {
-  let translations = HashMap::from([
-    ("en", include_str!("../i18n/en/base.json")),
-    ("fr", include_str!("../i18n/fr/base.json")),
-  ]);
 
-  let config = I18nProviderConfig {
-    default_language: "en".to_string(),
-    translations,
-    ..Default::default()
-  };
+  let config = use_memo((), |_| {
+    let translations = HashMap::from([
+      ("en", include_str!("../i18n/en/base.json")),
+      ("fr", include_str!("../i18n/fr/base.json")),
+    ]);
+
+    I18nProviderConfig {
+      default_language: "en".to_string(),
+      translations,
+      ..Default::default()
+    }
+  });
 
   html! {
-    <BrowserRouter>
-      <I18nProvider ..config>
+    <I18nProvider ..(*config).clone()>
+      <BrowserRouter>
         <div class="app">
           <Switch<AppRoute> render={switch}/>
         </div>
-      </I18nProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </I18nProvider>
   }
 }
