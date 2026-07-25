4a92b95d2ef77edd49fab677535f4396eaa50b7e
Emii-lia <fiaromiangaly@gmail.com>
2026-07-25 18:20:06 +0300

    perf(tailwind-config): adding scss to content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx,rs}",
+    "./src/**/*.{js,jsx,ts,tsx,rs,scss}",
     "index.html"
   ],
   theme: {
